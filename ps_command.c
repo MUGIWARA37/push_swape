d86dc2a60b65e22d0f8c56129ed3c4a5419a5191
Rida hlou <mugiwara@192.168.1.118>
2026-01-24 19:56:49 +0100

    create the moves forgot rr

---
+++ ps_command.c
@@ -0,0 +1,114 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ps_command.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhlou <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/24 19:14:49 by rhlou             #+#    #+#             */
+/*   Updated: 2026/01/24 19:56:33 by rhlou            ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#include "push_swap.h"
+
+void     swap_f2(t_stack **stack) // swap first 2 elements
+{
+    t_stack     *tmp;
+
+    tmp = (*stack)->next;
+    (*stack)->next = tmp->next;
+    tmp->next = *stack;
+    *stack = tmp;
+}
+
+void    double_swap(t_stack **stack_a , t_stack **stack_b)
+{
+    if(stack_a == stack_b)
+    {
+        swap_f2(stack_a);
+        return;
+    }
+    swap_f2(stack_a);
+    swap_f2(stack_b);
+}
+
+void    push_a(t_stack **a, t_stack **b)
+{
+    t_stack *tmp;
+
+    if (!b || !*b)
+        return;
+    tmp = *b;
+    *b = (*b)->next;
+    tmp->next = *a;
+    *a = tmp;
+}
+
+void    push_b(t_stack **a, t_stack **b)
+{
+    t_stack *tmp;
+
+    if (!a || !*a)
+        return;
+    tmp = *a;
+    *a = (*a)->next;
+    tmp->next = *b;
+    *b = tmp;
+}
+
+void    rotate_mf(t_stack **stack)
+{
+    t_stack *first;
+    t_stack *last;
+
+    if (!stack || !*stack || !(*stack)->next)
+        return;
+    first = *stack;
+    *stack = first->next;
+    first->next = NULL;
+    last = *stack;
+    while (last->next)
+        last = last->next;
+    last->next = first;
+}
+
+void    double_rotate_mn(t_stack **a,t_stack **b)
+{
+    if(a == b)
+    {
+        rotate_mf(a);
+        return ;
+    }
+    rotate_mf(a);
+    rotate_mf(b);
+}
+
+void    reverse_rotate_mf(t_stack **stack)
+{
+    t_stack *prev;
+    t_stack *last;
+
+    if (!stack || !*stack || !(*stack)->next)
+        return;
+    last = *stack;
+    while (last->next)
+    {
+        prev = last;
+        last = last->next;
+    }
+    prev->next = NULL;
+    last->next = *stack;
+    *stack = last;
+}
+
+void      double_rrr_mf(t_stack    **a,t_stack    **b)
+{
+    if(a == b)
+    {
+        reverse_rotate_mf(a);
+        return ;
+    }
+    reverse_rotate_mf(a);
+    reverse_rotate_mf(b);
+}
